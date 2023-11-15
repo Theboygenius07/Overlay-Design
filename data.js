a90c75b8ca4b0e5907fb44c41f6e6596d78e1342
Seyi Ogundipe <ogemgoldsmith07@gmail.com>
2023-11-16 00:10:35 +0100

    second

---
+++ data.js
@@ -0,0 +1,75 @@
+const data = [
+    {
+      name: "Sleek and Advanced Designs",
+      category: "Fashion",
+      link: "#",
+      copy: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta perferendis assumenda cupiditate placeat quidem cumque vel iusto delectus eaque minus.",
+      imgSrc: "hero.jpg",
+    },
+    {
+      name: "Nanotech Fabrics",
+      category: "Fashion",
+      link: "#",
+      copy: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta perferendis assumenda cupiditate placeat quidem cumque vel iusto delectus eaque minus.",
+      imgSrc: "https://img.freepik.com/free-photo/blurred-shiny-textured-silver-soft-feather_23-2148114528.jpg?w=740&t=st=1699192733~exp=1699193333~hmac=acd1a4fea22a7eb4172bfaebc78bc52e61cc1d9ff779a952ee11ef4c614db61c",
+    },
+    {
+      name: "Atmospheric Aero-adaption",
+      category: "Technology",
+      link: "#",
+      copy: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta perferendis assumenda cupiditate placeat quidem cumque vel iusto delectus eaque minus.",
+      imgSrc: "https://images.unsplash.com/photo-1533907650686-70576141c030?auto=format&fit=crop&q=80&w=1470&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "3D Holographic Printing",
+      category: "Technology",
+      link: "#",
+      copy: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta perferendis assumenda cupiditate placeat quidem cumque vel iusto delectus eaque minus.",
+      imgSrc: "hero.jpg",
+    },
+    {
+      name: "AR Shopping Experiences",
+      category: "Technology",
+      link: "#",
+      copy: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta perferendis assumenda cupiditate placeat quidem cumque vel iusto delectus eaque minus.",
+      imgSrc: "hero.jpg",
+    },
+    {
+      name: "Dynamic Morphing Clothing",
+      category: "Fashion",
+      link: "#",
+      copy: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta perferendis assumenda cupiditate placeat quidem cumque vel iusto delectus eaque minus.",
+      imgSrc: "hero.jpg",
+    },
+    {
+      name: "Carbon-Neutral Homes",
+      category: "Environment",
+      link: "#",
+      copy: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta perferendis assumenda cupiditate placeat quidem cumque vel iusto delectus eaque minus.",
+      imgSrc: "hero.jpg",
+    },
+    {
+      name: "Synthetic Products",
+      category: "Technology",
+      link: "#",
+      copy: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta perferendis assumenda cupiditate placeat quidem cumque vel iusto delectus eaque minus.",
+      imgSrc: "hero.jpg",
+    },
+    {
+      name: "Sustainable Zero-Waste",
+      category: "Environment",
+      link: "#",
+      copy: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta perferendis assumenda cupiditate placeat quidem cumque vel iusto delectus eaque minus.",
+      imgSrc: "hero.jpg",
+    },
+    {
+      name: "VR Fashion",
+      category: "Technology",
+      link: "#",
+      copy: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta perferendis assumenda cupiditate placeat quidem cumque vel iusto delectus eaque minus.",
+      imgSrc: "hero.jpg",
+    },
+  ];
+  
+  export { data };
+  
