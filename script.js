886b58589bb677848b7517404aa6c9f89a2c7b26
Seyi Ogundipe <ogemgoldsmith07@gmail.com>
2024-01-07 02:03:11 +0100

    com

--- script.js
+++ script.js
@@ -5,114 +5,114 @@ const itemDetails = document.querySelector(".items-details");
 
 
 //first set of animations
-document.addEventListener("DOMContentLoaded", function()
-{
-  const counter3 = document.querySelector(".counter-3");
+// document.addEventListener("DOMContentLoaded", function()
+// {
+//   const counter3 = document.querySelector(".counter-3");
   
-  for(let i = 0; i < 2; i++) {
-    for(let j = 0; j < 10; j++) {
-      const div = document.createElement("div");
-      div.className = "num";
-      div.textContent = j;
-      counter3.appendChild(div);
-    }
-  }
+//   for(let i = 0; i < 2; i++) {
+//     for(let j = 0; j < 10; j++) {
+//       const div = document.createElement("div");
+//       div.className = "num";
+//       div.textContent = j;
+//       counter3.appendChild(div);
+//     }
+//   }
 
-  const finalDiv = document.createElement("div");
-  finalDiv.className = "num";
-  finalDiv.textContent = "0";
-  counter3.appendChild(finalDiv);
+//   const finalDiv = document.createElement("div");
+//   finalDiv.className = "num";
+//   finalDiv.textContent = "0";
+//   counter3.appendChild(finalDiv);
 
-  function animate(counterElement, duration, delay = 0) {
-    const numHeight = counterElement.querySelector(".num").clientHeight;
+//   function animate(counterElement, duration, delay = 0) {
+//     const numHeight = counterElement.querySelector(".num").clientHeight;
   
-    const totalDistance = (counterElement.querySelectorAll(".num").length - 1) * numHeight;
+//     const totalDistance = (counterElement.querySelectorAll(".num").length - 1) * numHeight;
   
-    gsap.to(counterElement, {
-      y: -totalDistance,
-      duration: duration,
-      delay: delay,
-      ease: "power2.inOut",
-    });
-  }
+//     gsap.to(counterElement, {
+//       y: -totalDistance,
+//       duration: duration,
+//       delay: delay,
+//       ease: "power2.inOut",
+//     });
+//   }
   
 
-animate(counter3, 5);
-animate(document.querySelector(".counter-2"), 6);
-animate(document.querySelector(".counter-1"), 2, 4);
-});
+// animate(counter3, 5);
+// animate(document.querySelector(".counter-2"), 6);
+// animate(document.querySelector(".counter-1"), 2, 4);
+// });
 
-gsap.to(".digit", {  // Add a period before "digit"
-  top: "-150px",
-  stagger: {
-    amount: 0.25,
-  },
-  delay: 6,
-  duration: 1,
-  ease: "power4.inOut",
-});
+// gsap.to(".digit", {  // Add a period before "digit"
+//   top: "-150px",
+//   stagger: {
+//     amount: 0.25,
+//   },
+//   delay: 6,
+//   duration: 1,
+//   ease: "power4.inOut",
+// });
 
-gsap.from(".loader-1", {
-  width: 0,
-  duration: 6,
-  ease: "power2.inOut",
+// gsap.from(".loader-1", {
+//   width: 0,
+//   duration: 6,
+//   ease: "power2.inOut",
 
-});
+// });
 
-gsap.from(".loader-2", {
-  width: 0,
-  delay: 1.9,
-  duration: 3,
-  ease: "power2.inOut",
+// gsap.from(".loader-2", {
+//   width: 0,
+//   delay: 1.9,
+//   duration: 3,
+//   ease: "power2.inOut",
 
-});
+// });
 
-gsap.to(".loader", {
-  background: "none",
-  delay: 6,
-  duration: 0.1,
-});
+// gsap.to(".loader", {
+//   background: "none",
+//   delay: 6,
+//   duration: 0.1,
+// });
 
-gsap.to(".loader-1", {
-  rotate: 90,
-  y: -50,
-  delay: 6,
-  duration: 0.5,
-});
+// gsap.to(".loader-1", {
+//   rotate: 90,
+//   y: -50,
+//   delay: 6,
+//   duration: 0.5,
+// });
 
-gsap.to(
-  ".loader-2",
-  {
-    x: -75,
-    y: 75,
-    duration: 0.5,
-  },
-  "<"
-);
+// gsap.to(
+//   ".loader-2",
+//   {
+//     x: -75,
+//     y: 75,
+//     duration: 0.5,
+//   },
+//   "<"
+// );
 
 
-gsap.to(".loader", {
-  scale: 40,
-  duration: 1,
-  delay: 7,
-  ease: "power2.inOut",
-});
+// gsap.to(".loader", {
+//   scale: 40,
+//   duration: 1,
+//   delay: 7,
+//   ease: "power2.inOut",
+// });
 
-gsap.to(".loader", {
- rotate: 45,
- y: 500,
- x: 2000,
-  duration: 1,
-  delay: 7,
-  ease: "power2.inOut",
-});
+// gsap.to(".loader", {
+//  rotate: 45,
+//  y: 500,
+//  x: 2000,
+//   duration: 1,
+//   delay: 7,
+//   ease: "power2.inOut",
+// });
 
-gsap.to(".loading-screen", {
-opacity: 0,
-  duration: 0.5,
-  delay: 7.5,
-  ease: "power1.inOut",
-});
+// gsap.to(".loading-screen", {
+// opacity: 0,
+//   duration: 0.5,
+//   delay: 7.5,
+//   ease: "power1.inOut",
+// });
 
 // gsap.to(".bigContainer", {
 //   delay: 7,
